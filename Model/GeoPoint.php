c8d8dedddbf40294dad529e3142272ff16e8af5d
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-12-14 05:03:46 +0100

    GeoPoint implementation.

--- Model/GeoPoint.php
+++ Model/GeoPoint.php
@@ -1,9 +1,27 @@
 <?php
-
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
 namespace Smile\Map\Model;
 
 use Smile\Map\Api\Data\GeoPointInterface;
 
+/**
+ * Default implementation of the GeoPointInterface.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
 class GeoPoint implements GeoPointInterface
 {
     /**
@@ -17,9 +35,10 @@ class GeoPoint implements GeoPointInterface
     private $longitude;
 
     /**
+     * Constructor.
      *
-     * @param float $latitude
-     * @param float $longitude
+     * @param float $latitude  Latitude
+     * @param float $longitude Longitude
      */
     public function __construct($latitude, $longitude)
     {
@@ -27,13 +46,19 @@ class GeoPoint implements GeoPointInterface
         $this->longitude = $longitude;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getLatitude()
     {
-        return $this->getLatitude();
+        return $this->latitude;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getLongitude()
     {
-        return $this->getLongitude();
+        return $this->longitude;
     }
 }
