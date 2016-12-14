094fde54f6f4688090077bbd145fdc1bc4752d7f
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-12-14 05:10:29 +0100

    Coding standards / Code quality.

--- Api/MapProviderInterface.php
+++ Api/MapProviderInterface.php
@@ -1,10 +1,30 @@
 <?php
-
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
 namespace Smile\Map\Api;
 
+/**
+ * Map provider interface definition.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
 interface MapProviderInterface
 {
     /**
+     * Return configured map.
+     *
      * @return MapInteface
      */
     public function getMap();
--- Model/MapProvider.php
+++ Model/MapProvider.php
@@ -1,10 +1,28 @@
 <?php
-
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
 namespace Smile\Map\Model;
 
 use Smile\Map\Api\MapProviderInterface;
 use Smile\Map\Helper\Map as MapHelper;
 
+/**
+ * Default implementation of the MapProviderInterface.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
 class MapProvider implements MapProviderInterface
 {
     /**
@@ -20,7 +38,8 @@ class MapProvider implements MapProviderInterface
     /**
      * Contructor.
      *
-     * @param \Smile\Map\Api\MapInterface[] $mapProviders
+     * @param MapHelper $mapHelper    Map helper.
+     * @param array     $mapProviders Map providers.
      */
     public function __construct(MapHelper $mapHelper, array $mapProviders = [])
     {
@@ -28,6 +47,9 @@ class MapProvider implements MapProviderInterface
         $this->mapProviders = $mapProviders;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getMap()
     {
         $providerIdentifier = $this->mapHelper->getProviderIdentifier();
