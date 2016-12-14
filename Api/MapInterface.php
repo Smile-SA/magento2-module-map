9619b49270a96394aab83affcf5032692aa36773
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-12-14 05:09:28 +0100

    Map direction implementation (WIP).

--- Api/MapInterface.php
+++ Api/MapInterface.php
@@ -1,16 +1,50 @@
 <?php
-
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
 namespace Smile\Map\Api;
 
+use Smile\Map\Api\Data\GeoPointInterface;
+
+/**
+ * Map interface definition.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
 interface MapInterface
 {
     /**
+     * Returns current map provider identifier.
+     *
      * @return string
      */
     public function getIdentifier();
 
     /**
+     * Return current map configuration.
+     *
      * @return array
      */
     public function getConfig();
+
+    /**
+     * Returns the direction URL using the current provider.
+     *
+     * @param GeoPointInterface $dest Destination for the direction URL.
+     * @param GeoPointInterface $orig Optional origin for the direction URL.
+     *
+     * @return string
+     */
+    public function getDirectionUrl(GeoPointInterface $dest, GeoPointInterface $orig = null);
 }
--- Model/Map/DefaultMap.php
+++ Model/Map/DefaultMap.php
@@ -1,28 +1,91 @@
 <?php
-
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
 namespace Smile\Map\Model\Map;
 
 use \Smile\Map\Helper\Map as MapHelper;
+use Magento\Framework\Filter\FilterManager;
+use Smile\Map\Api\Data\GeoPointInterface;
 
+/**
+ * Default implementation of the MapInterface.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
 class DefaultMap implements \Smile\Map\Api\MapInterface
 {
+    /**
+     * @var string
+     */
     private $identifier;
 
+    /**
+     * @var array
+     */
     private $config;
 
-    public function __construct($identifier, MapHelper $mapHelper)
+    /**
+     * @var FilterManager
+     */
+    private $filterManager;
+
+    /**
+     * Constructor.
+     *
+     * @param string        $identifier    Map identifier.
+     * @param MapHelper     $mapHelper     Map helper.
+     * @param FilterManager $filterManager Template filter manager.
+     */
+    public function __construct($identifier, MapHelper $mapHelper, FilterManager $filterManager)
     {
-        $this->identifier = $identifier;
-        $this->config     = $mapHelper->getProviderConfiguration($identifier);
+        $this->identifier    = $identifier;
+        $this->config        = $mapHelper->getProviderConfiguration($identifier);
+        $this->filterManager = $filterManager;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getConfig()
     {
         return $this->config;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getIdentifier()
     {
         return $this->identifier;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDirectionUrl(GeoPointInterface $dest, GeoPointInterface $orig = null)
+    {
+        $urlTemplate = $this->config['direction_url_template'];
+
+        $data = new \Magento\Framework\DataObject();
+        $data->setDest(['latitude' => $dest->getLatitude(), 'longitude' => $dest->getLongitude()]);
+
+        if ($orig !== null) {
+            $data->setOrig(['latitude' => $orig->getLatitude(), 'longitude' => $orig->getLongitude()]);
+        }
+        // @todo
+        //return $this->filterManager->template($urlTemplate, ['variables' => $data->toArray()]);
+        return '';
     }
 }
