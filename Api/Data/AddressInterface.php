1d49afe301eed53c01271b930c2fc97c858b0cd6
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-12-14 05:04:16 +0100

    Addresses implementation.

--- Api/Data/AddressInterface.php
+++ Api/Data/AddressInterface.php
@@ -1,8 +1,130 @@
 <?php
-
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
 namespace Smile\Map\Api\Data;
 
+/**
+ * Address interface definition.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
 interface AddressInterface
 {
+    /**#@+
+     * Constants for keys of data array. Identical to the name of the getter in snake case
+     */
+    const STREET     = 'street';
+    const POSTCODE   = 'postcode';
+    const CITY       = 'city';
+    const REGION     = 'region';
+    const REGION_ID  = 'region_id';
+    const COUNTRY_ID = 'country_id';
 
+    /**
+     * Get region.
+     *
+     * @return \Magento\Customer\Api\Data\RegionInterface|null
+     */
+    public function getRegion();
+
+    /**
+     * Get region ID.
+     *
+     * @return int|null
+     */
+    public function getRegionId();
+
+    /**
+     * Two-letter country code in ISO_3166-2 format.
+     *
+     * @return string|null
+     */
+    public function getCountryId();
+
+    /**
+     * Get street.
+     *
+     * @return string[]|null
+     */
+    public function getStreet();
+
+    /**
+     * Get postcode.
+     *
+     * @return string|null
+     */
+    public function getPostcode();
+
+    /**
+     * Get city name.
+     *
+     * @return string|null
+     */
+    public function getCity();
+
+    /**
+     * Set country id.
+     *
+     * @param string $countryId Country id.
+     *
+     * @return $this
+     */
+    public function setCountryId($countryId);
+
+    /**
+     * Set region.
+     *
+     * @param \Magento\Customer\Api\Data\RegionInterface $region Region.
+     *
+     * @return $this
+     */
+    public function setRegion(\Magento\Customer\Api\Data\RegionInterface $region = null);
+
+    /**
+     * Set region ID.
+     *
+     * @param int $regionId Region id.
+     *
+     * @return $this
+     */
+    public function setRegionId($regionId);
+
+    /**
+     * Set street.
+     *
+     * @param string[] $street Street.
+     *
+     * @return $this
+     */
+    public function setStreet(array $street);
+
+    /**
+     * Set postcode.
+     *
+     * @param string $postcode Postcode.
+     *
+     * @return $this
+     */
+    public function setPostcode($postcode);
+
+    /**
+     * Set city name.
+     *
+     * @param string $city City name.
+     *
+     * @return $this
+     */
+    public function setCity($city);
 }
---
+++ Model/Address.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Apache License Version 2.0
+ */
+namespace Smile\Map\Model;
+
+use Smile\Map\Api\Data\AddressInterface;
+use Magento\Framework\Model\AbstractModel;
+
+/**
+ * Default implementation of the AddressInterface.
+ *
+ * @category Smile
+ * @package  Smile\Map
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class Address extends AbstractModel implements AddressInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getCountryId()
+    {
+        return $this->getData(self::COUNTRY_ID);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRegion()
+    {
+        return $this->getData(self::REGION);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRegionId()
+    {
+        return $this->getData(self::REGION_ID);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getStreet()
+    {
+        return $this->getData(self::STREET);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPostcode()
+    {
+        return $this->getData(self::POSTCODE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCity()
+    {
+        return $this->getData(self::CITY);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setRetailerId($retailerId)
+    {
+        return $this->setData(self::RETAILER_ID, $retailerId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setCountryId($countryId)
+    {
+        return $this->setData(self::COUNTRY_ID, $countryId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setRegion(\Magento\Customer\Api\Data\RegionInterface $region = null)
+    {
+        return $this->setData(self::REGION, $region);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setRegionId($regionId)
+    {
+        return $this->setData(self::REGION_ID, $regionId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setStreet(array $street)
+    {
+        return $this->setData(self::STREET, $street);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setPostcode($postcode)
+    {
+        return $this->setData(self::POSTCODE, $postcode);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setCity($city)
+    {
+        return $this->setData(self::CITY, $city);
+    }
+}
